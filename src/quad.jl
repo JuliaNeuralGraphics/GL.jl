68e1a0282f3c3282d9e0451c1a60ce86aab1a45c
Anton Smirnov <tonysmn97@gmail.com>
2022-11-15 22:59:34 +0200

    Use singe texture sampler

--- src/quad.jl
+++ src/quad.jl
@@ -2,7 +2,6 @@ struct QuadVertex
     position::SVector{3, Float32}
     color::SVector{4, Float32}
     texture_coordinate::SVector{2, Float32}
-    texture_slot::Float32
 end
 
 mutable struct RenderSurface
@@ -29,16 +28,13 @@ function get_program(::Type{RenderSurface})
     layout (location = 0) in vec3 a_Position;
     layout (location = 1) in vec4 a_Color;
     layout (location = 2) in vec2 a_TexCoord;
-    layout (location = 3) in float a_TexId;
 
     out vec4 v_Color;
     out vec2 v_TexCoord;
-    out float v_TexId;
 
     void main() {
         v_Color = a_Color;
         v_TexCoord = a_TexCoord;
-        v_TexId = a_TexId;
         gl_Position = vec4(a_Position, 1.0);
     }
     """
@@ -49,13 +45,13 @@ function get_program(::Type{RenderSurface})
     in vec2 v_TexCoord;
     in float v_TexId;
 
-    uniform sampler2D u_Textures[1];
+    uniform sampler2D u_Texture;
 
     layout (location = 0) out vec4 color;
 
     void main() {
         color = v_Color;
-        color *= texture(u_Textures[int(v_TexId)], v_TexCoord);
+        color *= texture(u_Texture, v_TexCoord);
     }
     """
     ShaderProgram((
@@ -77,8 +73,7 @@ end
 
 @inline set_data!(s::RenderSurface, data) = set_data!(s.texture, data)
 
-"Return a quad vertex array, used to display the rendered NeRF."
-function get_quad_va(tint = ones(SVector{4, Float32}), texture_slot = 0f0)
+function get_quad_va(tint = ones(SVector{4, Float32}))
     uvs = (
         SVector{2, Float32}(0f0, 0f0), SVector{2, Float32}(1f0, 0f0),
         SVector{2, Float32}(1f0, 1f0), SVector{2, Float32}(0f0, 1f0))
@@ -88,13 +83,12 @@ function get_quad_va(tint = ones(SVector{4, Float32}), texture_slot = 0f0)
         SVector{3, Float32}( 1f0,  1f0, 0f0),
         SVector{3, Float32}(-1f0,  1f0, 0f0))
 
-    data = [QuadVertex(vertices[i], tint, uvs[i], texture_slot) for i in 1:4]
+    data = [QuadVertex(vertices[i], tint, uvs[i]) for i in 1:4]
 
     layout = BufferLayout([
         BufferElement(SVector{3, Float32}, "a_Position"),
         BufferElement(SVector{4, Float32}, "a_Color"),
-        BufferElement(SVector{2, Float32}, "a_TextureCoordinate"),
-        BufferElement(SVector{1, Float32}, "a_TextureId")])
+        BufferElement(SVector{2, Float32}, "a_TextureCoordinate")])
 
     ib = IndexBuffer(UInt32[0, 1, 2, 2, 3, 0])
     vb = VertexBuffer(data, layout)
